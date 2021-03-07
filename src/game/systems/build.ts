989a38d1215788db722b61241aab68cdc873382a
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 17:02:21 +0100

    Check resources and charge for building factories

--- src/game/systems/build.ts
+++ src/game/systems/build.ts
@@ -5,24 +5,37 @@ const id = (seed = 0) => (prefix = "") => `${prefix}${++seed}`;
 
 const factoryId = ((id) => () => id("factory"))(id(0));
 
-const spawnSystem = (entities: Entities) => {
-    const { mouseController, map } = entities;
+const factoryPrice = {
+    escudos: 300,
+    wood: 10,
+} as const;
+
+const buildSystem = (entities: Entities) => {
+    const { mouseController, map, resources } = entities;
 
     if (mouseController.left && !mouseController.previous?.left) {
         const clickedPosition = mouseController.position;
 
-        if (
+        const clickedOnMap =
             map?.width != null &&
             map?.height &&
             clickedPosition.x < map.width &&
-            clickedPosition.y < map.height
-        )
+            clickedPosition.y < map.height;
+
+        const hasResources =
+            resources.escudos >= factoryPrice.escudos &&
+            resources.wood >= factoryPrice.wood;
+
+        if (clickedOnMap && hasResources) {
+            resources.escudos -= factoryPrice.escudos;
+            resources.wood -= factoryPrice.wood;
             entities[factoryId()] = factoryEntity({
                 ...clickedPosition,
             });
+        }
     }
 
     return entities;
 };
 
-export default spawnSystem;
+export default buildSystem;
--- src/game/systems/index.ts
+++ src/game/systems/index.ts
@@ -1,10 +1,10 @@
 import { GameEngineSystem } from "react-game-engine";
 import { Entities } from "../entities";
+import buildSystem from "./build";
 import hudSystem from "./hud";
 import incomeSystem from "./income";
 import keyboardControllerSystem from "./keyboard-controller";
 import mouseControllerSystem from "./mouse-controller";
-import spawnSystem from "./spawn";
 // import movementSystem from "./movement";
 
 // If we use the current solution that modifies the updateArgs object, the order is very important.
@@ -12,7 +12,7 @@ const systems: GameEngineSystem<Entities>[] = [
     keyboardControllerSystem, // Needs to be first to feed subsequent systems with keys
     mouseControllerSystem,
     hudSystem,
-    spawnSystem,
+    buildSystem,
     incomeSystem,
     // movementSystem,
 ];
