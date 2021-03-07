c60e230f81bdb9770a0982d3d8578ecc62e34973
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 03:17:17 +0100

    Make game fill full height so we can't unfocus it

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -2,10 +2,17 @@ import { Component } from "react";
 import { GameEngine } from "react-game-engine";
 import Entities from "./entities";
 import systems from "./systems";
+import style from "./game.module.css";
 
 class Game extends Component {
     render() {
-        return <GameEngine systems={systems} entities={Entities()} />;
+        return (
+            <GameEngine
+                className={style.game}
+                systems={systems}
+                entities={Entities()}
+            />
+        );
     }
 }
 
