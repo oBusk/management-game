a52681ea54a6356490e915b79894e2607bb0e86f
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 03:58:18 +0100

    Fix HUD not showing keys

--- src/game/systems/index.ts
+++ src/game/systems/index.ts
@@ -2,6 +2,11 @@ import HUD from "./hud";
 import KeyboardController from "./keyboard-controller";
 import Movement from "./movement";
 
-const systems = [HUD, KeyboardController(), Movement];
+// If we use the current solution that modifies the updateArgs object, the order is very important.
+const systems = [
+    KeyboardController(), // Needs to be first to feed subsequent systems with keys
+    HUD,
+    Movement,
+];
 
 export default systems;
