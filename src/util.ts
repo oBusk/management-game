1a5cc20e2e5b24c9d7c32e24c79a2b50fc4d817b
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 23:10:11 +0100

    Fix canAfford to miscalculate wood prices

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,9 @@
 import { MapEntity } from "./game/entities/map";
 import { Resources } from "./resources";
 
-export const canAfford = (resources: Resources, price: Resources): boolean => {
-    return (
-        (price.escudos ?? Number.MAX_SAFE_INTEGER) <=
-            (resources.escudos ?? 0) &&
-        (price.wood ?? Number.MAX_SAFE_INTEGER) <= (price.wood ?? 0)
-    );
-};
+export const canAfford = (resources: Resources, price: Resources): boolean =>
+    (price.escudos ?? Number.MAX_SAFE_INTEGER) <= (resources.escudos ?? 0) &&
+    (price.wood ?? Number.MAX_SAFE_INTEGER) <= (resources.wood ?? 0);
 
 export const isWithinMap = (
     map: MapEntity,
