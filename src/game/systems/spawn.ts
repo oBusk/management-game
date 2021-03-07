046cdb877400c57b6bf9bca84f43cbac5befec4b
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 16:52:53 +0100

    Check if you clicked on the map at least

--- src/game/systems/spawn.ts
+++ src/game/systems/spawn.ts
@@ -11,23 +11,15 @@ const spawnSystem = (entities: Entities) => {
     if (mouseController.left && !mouseController.previous?.left) {
         const clickedPosition = mouseController.position;
 
-        // const size = 80;
-        // const topLeftCorner = {
-        //     x: clickedPosition.x - size / 2,
-        //     y: clickedPosition.y - size / 2,
-        // };
-
-        // if (
-        //     topLeftCorner.x > 0 &&
-        //     topLeftCorner.y > 0 &&
-        //     map?.width != null &&
-        //     map?.height != null &&
-        //     topLeftCorner.x + size < map.width &&
-        //     topLeftCorner.y + size < map.height
-        // )
-        entities[factoryId()] = factoryEntity({
-            ...clickedPosition,
-        });
+        if (
+            map?.width != null &&
+            map?.height &&
+            clickedPosition.x < map.width &&
+            clickedPosition.y < map.height
+        )
+            entities[factoryId()] = factoryEntity({
+                ...clickedPosition,
+            });
     }
 
     return entities;
