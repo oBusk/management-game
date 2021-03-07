a2f9ce4f5895498b0a2118a89a1c8343f8b2d680
Oscar Busk <oscar.busk@gmail.com>
2021-03-07 15:03:31 +0100

    Only reset when pressed

--- src/game/systems/movement.ts
+++ src/game/systems/movement.ts
@@ -6,14 +6,14 @@ const movementSystem = (entities: Entities) => {
     const { box, keyboardController } = entities;
 
     if (box && keyboardController) {
-        const { w, a, s, d, space, control } = keyboardController;
+        const { w, a, s, d, space, control, previous } = keyboardController;
 
         if (w || a || s || d) {
             box.y = (box.y ?? 200) + (s ? 1 : w ? -1 : 0) * MOVEMENT_SPEED;
             box.x = (box.x ?? 200) + (d ? 1 : a ? -1 : 0) * MOVEMENT_SPEED;
         }
 
-        if (space || control) {
+        if ((space && !previous?.space) || (control && !previous?.control)) {
             // reset i guess?
             box.y = box.x = 200;
         }
